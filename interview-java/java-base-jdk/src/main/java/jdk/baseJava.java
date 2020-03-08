496f17a9c0844ffd2285ca8ccb6a05290affdb89
sleeve <1421147715@qq.com>
2020-03-08 22:54:09 +0800

    源码阅读区

---
+++ interview-java/java-base-jdk/src/main/java/jdk/baseJava.java
@@ -0,0 +1,11 @@
+package jdk;
+
+public class baseJava {
+    public static void main(String[] args) {
+        String s = "sss";
+        StringBuffer stringBuffer = new StringBuffer("buffer");
+        StringBuilder stringBuilder = new StringBuilder("StringBuilder");
+
+
+    }
+}
