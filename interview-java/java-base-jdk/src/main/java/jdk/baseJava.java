5f80585af7fc44b0158825e190ebabb564577991
sleeve <1421147715@qq.com>
2020-03-09 23:26:46 +0800

    update 3.9

--- interview-java/java-base-jdk/src/main/java/jdk/baseJava.java
+++ interview-java/java-base-jdk/src/main/java/jdk/baseJava.java
@@ -1,11 +1,33 @@
 package jdk;
 
+import javax.swing.plaf.synth.SynthStyle;
+
 public class baseJava {
+    int a =2;
+    static int b = 3;
     public static void main(String[] args) {
-        String s = "sss";
+        String s = "sss2";
+        String s2= new String("sss2");
+        String s3= new String("sss2");
+
         StringBuffer stringBuffer = new StringBuffer("buffer");
         StringBuilder stringBuilder = new StringBuilder("StringBuilder");
-
+        StringBuffer stringBuffer2 = stringBuffer;
+        System.out.println(stringBuffer.hashCode()+"\n"+stringBuffer2.hashCode());
+        System.out.println("\n");
+        System.out.println(s.hashCode()+"\n"+s2.hashCode()+"\n"+s3.hashCode());
+        System.out.println(s3==s2);
 
     }
+    public static void staticA(){
+
+        System.out.println("测试静态方法");
+    }
+
+    public void instanceB(){
+
+        System.out.println("测试实例方法");
+    }
+
 }
+
